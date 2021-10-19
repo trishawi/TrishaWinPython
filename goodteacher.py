4513da0e5f2610eadbf490ce65d91ac53dbe81d3
trishawi <trisha.win@education.nsw.gov.au>
2021-10-20 09:45:32 +1100

    goodteacher1

---
+++ goodteacher.py
@@ -0,0 +1,12 @@
+print(""" ------------------------------------------------
+|                                                |
+|    04GoodTeacher                               |
+|    Name : Mr Horan                             |
+|    Version : 01                                |
+|                                                |
+ ------------------------------------------------""")
+teacher = input("Teacher's name (try Mr Horan) ")
+if teacher == "Mr Horan":
+    print("You are lucky, he is a great teacher.")
+else:
+    print(teacher + " is an ok teacher")
