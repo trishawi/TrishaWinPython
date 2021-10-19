0e9705aa76b51b2134fc9c4b33cc0ad7e37dc66a
trishawi <trisha.win@education.nsw.gov.au>
2021-10-20 09:40:47 +1100

    goodbyeperson1

---
+++ goodbyeperson.py
@@ -0,0 +1,10 @@
+print(""" ------------------------------------------------
+|                                                |
+|    03GoodbyePerson                             |
+|    Name : Trisha Win                           |
+|    Version : 01                                |
+|                                                |
+ ------------------------------------------------
+Hello""")
+name = input("What is your name ?")
+print("Goodbye " + name)
