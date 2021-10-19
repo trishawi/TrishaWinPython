0bbcf5d159b175957c21a4b24407ddbddb1ff5bc
trishawi <trisha.win@education.nsw.gov.au>
2021-10-19 21:28:54 +1100

    goodbyeworld1

---
+++ goodbye.py
@@ -0,0 +1,10 @@
+print(""" ------------------------------------------------
+|                                                |
+|    02GoodbyeWorld                              |
+|    Name : Trisha Win                           |
+|    Version : 01                                |
+|                                                |
+ ------------------------------------------------
+Hello World""")
+input("------> Program paused - press enter to continue")
+print("Goodbye World")
