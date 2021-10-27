9b9d772526ddacd9e4cf8a364af414f5589cb82f
trishawi <trisha.win@education.nsw.gov.au>
2021-10-28 10:50:41 +1100

    whileloop2

--- 07menu (unfinished).py
+++ 07menu (unfinished).py
@@ -5,29 +5,5 @@ print(""" ------------------------------------------------
 |    Version : 01                                |
 |                                                |
  ------------------------------------------------""")
-menu = {}
-menu['1']="Add Student." 
-menu['2']="Delete Student."
-menu['3']="Find Student"
-menu['4']="Exit"
-while True: 
-    options=menu.keys()
-    options.sort()
-    for entry in options: 
-      print (entry + menu[entry])
-
-    selection= input("Please Select:") 
-    if selection == "1": 
-      print("add") 
-    elif selection == "2": 
-      print("delete")
-    elif selection == "3":
-      print("find")
-    elif selection == '4': 
-      break
-    else: 
-      print ("""----Start of Output ---------------------------
-
-invalid option
-
-----End of Output -----------------------------""")
+def one():
+  print("Hello World")
