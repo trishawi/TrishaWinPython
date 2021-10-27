2dfab272ef9f5f998a9455cb99b37a06e9abbb6c
trishawi <trisha.win@education.nsw.gov.au>
2021-10-28 10:37:51 +1100

    whileloop1

---
+++ 06whileLoop.py
@@ -0,0 +1,24 @@
+print(""" ------------------------------------------------
+|                                                |
+|    06whileLoop                                 |
+|    Name : Trisha Win                           |
+|    Version : 01                                |
+|                                                |
+ ------------------------------------------------""")
+
+answer = ""
+
+while True:
+    answer = input("What is the name of this subject ")
+    if answer != "IST":
+        print("Not correct - try again")
+    else:
+        break
+
+print("""
+
+Congratulations!!
+
+
+""")
+
