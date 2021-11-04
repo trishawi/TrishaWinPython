3762a6c3659377d24a42d2487bc818d5d814b9a8
trishawi <trisha.win@education.nsw.gov.au>
2021-11-04 18:34:57 +1100

    menu2

---
+++ 07menu.py
@@ -0,0 +1,107 @@
+import os
+def hello_world():
+    print("""Hello World""")
+
+def goodbye_world():
+    print("Hello World")
+    input("------> Program paused - press enter to continue")
+    print("Goodbye World")
+
+def goodbye_person():
+    name1 = input("Hello World\nWhat is your name ? ")
+    print("Goodbye " + name1)
+
+def good_teacher():
+    teacher = input("Teacher's name (try Mr Horan) ")
+    if teacher == "Mr Horan":
+        print("You are lucky, he is a great teacher.")
+    else:
+        print(teacher + " is an ok teacher")
+
+def for_loop():
+    x = 0
+    while (x<499):
+        x = (x + 1)
+        print(x)
+
+def while_loop():
+    while True:
+        answer = input("What is the name of this subject ")
+        if answer != "IST":
+            print("Not correct - try again")
+        else:
+            break
+
+    print("\n\nCongratulations!!\n\n")
+
+def unknown():
+    print("invalid option")
+
+def x():
+    print()
+
+def start():
+    print("\n----Start of Output ---------------------------\n")
+
+def end():
+    print("\n----End of Output -----------------------------\n\n\n")
+
+def enter():
+    input("Press Enter to continue")
+
+answer = ""
+
+while answer != "x":
+    print(""" ------------------------------------------------
+    |                                                |
+    |    07Menu                                      |
+    |    Name : Trisha Win                           |
+    |    Version : 01                                |
+    |                                                |
+    ------------------------------------------------""")
+
+    os.system('cls')
+    print("""\n1. Hello World\n2. Goodbye World\n3. Goodbye Person\n4. Good Teacher\n5. forLoop\n6. whileLoop\n7. string Loop\n8. Convert to ascii\n9. Encode a string\nx. To Exit""")
+
+    answer = input("Enter an option ")
+
+    if answer == "1":
+        start()
+        hello_world()
+        end()
+        enter()
+    elif answer == "2":
+        start()
+        goodbye_world()
+        end()
+        enter()
+    elif answer == "3":
+        start()
+        goodbye_person()
+        end()
+        enter()
+    elif answer == "4":
+        start()
+        good_teacher()
+        end()
+        enter()
+    elif answer == "5":
+        start()
+        for_loop()
+        end()
+        enter()
+    elif answer == "6":
+        start()
+        while_loop()
+        end()
+        enter()
+    elif answer != "1" and answer != "2" and answer != "3" and answer != "4" and answer != "5" and answer != "6" and answer != "x":
+        start()
+        unknown()
+        end()
+        enter()
+else:
+    start()
+    x()
+    end()
+    enter()
