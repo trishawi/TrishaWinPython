9354262372717f1b95fc58f69fc94e4f2e7fd054
trishawi <trisha.win@education.nsw.gov.au>
2021-11-05 11:04:00 +1100

    extension2

--- Extension (unfinished).py
+++ Extension (unfinished).py
@@ -12,26 +12,66 @@ def number_guesser():
     guesses = 0
     while answer != x:
         guesses = guesses + 1
-        answer = input("Guess your number. ")
+        answer = input("Guess my number. ")
         if int(answer) < x:
             print("Your answer is too small.")
         elif int(answer) > x:
             print("Your answer is too large.")
         elif int(answer) == x:
             break
-    print("Hooray! You guessed the number in " + str(guesses) + " guesses!")
-    input("Press enter to return to the menu.")
-    
-        
+    print("Hooray! You guessed the number in " + str(guesses) + " guesses!") 
 
 def scissors_paper_rock():
+    win = 0
     print("Welcome to Scissors Paper Rock!\nHow to play:\nPick scissors, paper or rock and I will do the same. Good luck!")
     time.sleep(2)
     input("Press enter to start")
-    input("Press enter to return to the menu.")
+    choice = (input("Enter scissors, paper or rock. ")).lower()
+    mylist = ["scissors", "paper", "rock"]
+    x = (random.choice(mylist))
+    if choice == "scissors":
+        print("You chose scissors.")
+        if x == "scissors":
+            print("I chose scissors.")
+            win = 2
+        elif x == "paper":
+            print("I chose paper.")
+            win = 3
+        elif x == "rock":
+            print("I chose rock.")
+            win = 1
+    elif choice == "paper":
+        print("You chose scissors.")
+        if x == "scissors":
+            print("I chose scissors.")
+            win = 1
+        elif x == "paper":
+            print("I chose paper.")
+            win = 2
+        elif x == "rock":
+            print("I chose rock.")
+            win = 3
+    elif choice == "rock":
+        print("You chose rock.")
+        if x == "scissors":
+            print("I chose scissors.")
+            win = 3
+        elif x == "paper":
+            print("I chose paper.")
+            win = 1
+        elif x == "rock":
+            print("I chose rock.")
+            win = 2
+    if win == 1:
+        print("You lose.")
+    elif win == 2:
+        print("Draw!")
+    elif win == 3:
+        print("You win!")
 
 def invalid():
-    print("Invalid option.")
+    input("Invalid option. Press Enter to return.")
+    os.system('cls')
 
 def enter():
     input("Press Enter to return to the menu.")
@@ -46,16 +86,19 @@ print(""" ------------------------------------------------
 ------------------------------------------------""")
 name = input("What's your name? ")
 
+game = ""
 while game != "x":
-    print("Welcome to the python minigames, " + name + "! Choose a game below:\n1. Number Guesser\n2. Scissors Paper Rock")
+    print("Welcome to the python minigames, " + name + "! Choose a game below:\n1. Number Guesser\n2. Scissors Paper Rock\nx. Exit")
     time.sleep(0.5) 
     game = input("Enter your choice: ")
     if game == "1":
         number_guesser()
+        enter()
     elif game == "2":
         scissors_paper_rock()
-    else:
+        enter()
+    elif game != "1" and game != "2" and game != "x":
         invalid()
 
 print("Thanks for playing!")
-wait(0.5)
+time.sleep(0.5)
